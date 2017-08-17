3a0685004ff3ad06040b2c9443618a3b8cf6ad39
sp <samp.830@gmail.com>
2017-08-17 02:52:05 -0700

    initial commit

---
+++ Hello/Speaker.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <iostream>
+
+namespace Hello {
+
+class Speaker {
+public:
+void sayHello();
+};
+}
---
+++ build/CMakeFiles/CMakeHelloWorld.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CMakeHelloWorld.dir/demo.cpp.o"
+  "CMakeHelloWorld.pdb"
+  "CMakeHelloWorld"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CMakeHelloWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
