e967272f38f18581299554f4a2bff7d9d437badd
sp <samp.830@gmail.com>
2017-08-17 03:42:02 -0700

    added text file generation in demo.cpp

--- demo.cpp
+++ demo.cpp
@@ -1,9 +1,13 @@
 #include<Speaker.h>
+#include<iostream>
+#include<fstream>
 using namespace std;
 using namespace Hello;
 
 int main(int argc, char *argv[]){
 Speaker* speaker = new Speaker();
-
 speaker->sayHello();
+std :: ofstream outfile ("test.txt");
+outfile << "test text" << std :: endl;
+outfile.close();
 }
